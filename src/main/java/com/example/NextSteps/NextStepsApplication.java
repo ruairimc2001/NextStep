23763a74e467e4960462192e03a7b549926f291a
ruairimcconnell <ruairimcconnell@deloitte.co.uk>
2025-11-09 18:34:37 +0000

    Initial Spring boot project setup

---
+++ src/main/java/com/example/NextSteps/NextStepsApplication.java
@@ -0,0 +1,13 @@
+package com.example.NextSteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextStepsApplication.class, args);
+	}
+
+}
